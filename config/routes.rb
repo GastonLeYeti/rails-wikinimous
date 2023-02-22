6285d8c3f8a16c5172198e3383988b4e3089e307
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-02-22 09:17:26 +0100

    ✨ Des articles en rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Defines the route for the "articles" resource
+  root "articles#index"
+  # Read all
+  get    "articles", to: "articles#index"
+  # Create
+  get    "articles/new", o: "articles#new",  as: :new_article
+  post   "articles", to: "articles#create"
+  # Show
+  get    "articles/:id", to: "articles#show", as: :article
+  # Update
+  get    "articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch  "articles/:id", to: "articles#update"
+  # Delete
+  delete "articles/:id", to: "articles#destroy"
+
 end
